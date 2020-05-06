630c6c05f03f2de47bdbcb8bda0b185912a4d528
Suvankar <suv.das19@gmail.com>
2020-05-06 20:30:19 +0530

    Live Server Publishing

--- lib/core/constants/api_constats.dart
+++ lib/core/constants/api_constats.dart
@@ -1,13 +1,13 @@
-//const String organizationServer =
-//    "https://scripsorganizationapi20191204032750.azurewebsites.net";
-const String organizationServer = "http://52.25.96.244:7014";
+const String organizationServer =
+    "https://scripsorganizationapi20191204032750.azurewebsites.net";
+//const String organizationServer = "http://52.25.96.244:7014";
 
-//const String practiceServer = "http://scripspracticesetup.azurewebsites.net";
-const String practiceServer = "http://52.25.96.244:7008";
+const String practiceServer = "http://scripspracticesetup.azurewebsites.net";
+//const String practiceServer = "http://52.25.96.244:7008";
 
-//const String questionBankServer =
-//    "http://scripsqueestionbank.azurewebsites.net";
-const String questionBankServer = "http://52.25.96.244:7020";
+const String questionBankServer =
+    "http://scripsqueestionbank.azurewebsites.net";
+//const String questionBankServer = "http://52.25.96.244:7020";
 
-const String patientsServer = "http://52.25.96.244:7017";
-//const String patientsServer = "https://scripspatients.azurewebsites.net";
+//const String patientsServer = "http://52.25.96.244:7017";
+const String patientsServer = "https://scripspatients.azurewebsites.net";
